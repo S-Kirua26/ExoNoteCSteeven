135db8c31d7a024a19b020ac75fd635929e2f03d
S-Kirua26 <steevengheeraert26@gmail.com>
2022-03-29 16:54:56 +0200

    serieProjet

--- ProjetPartie1/BusinessLogic/Operation.cs
+++ ProjetPartie1/BusinessLogic/Operation.cs
@@ -11,6 +11,7 @@ namespace CompteBancaire.BusinessLogic
     {
         public string Sortie { get; set; }
         public List<string> listeEtat;
+
         public Operation(string sortie)
         {
             Sortie = sortie;
@@ -20,10 +21,12 @@ namespace CompteBancaire.BusinessLogic
         public string ResultatOperation(Dictionary<string, double> dicoComptes, List<Transaction> listTransa, string sortie)
         {
             string affichage = "";
+            List<string> listeDoublon = new List<string>();
 
+            Console.WriteLine($"Voici la liste des comptes :");
             foreach (var item in dicoComptes)
             {
-                Console.WriteLine($"Voici la liste des comptes : {item.Key} _ {item.Value}");
+                Console.WriteLine($"{item.Key} _ {item.Value}");
             }
 
             foreach (Transaction transaction in listTransa)
@@ -37,68 +40,78 @@ namespace CompteBancaire.BusinessLogic
                 //        Console.WriteLine($"La transcation {transaction.IdTransaction} est en double");
                 //    }
                 //}
-                if (transaction.Montant < 0)
+                //for (int i = 1; i < listeDoublon.Count; i++)
+                //{
+                //    if (listeDoublon[i] == transaction.IdTransaction)
+                //    {
+                //        affichage = transaction.IdTransaction + " :KO";
+                //        listeEtat.Add(affichage);
+                //        Console.WriteLine($"La transaction {transaction.IdTransaction} est en double");
+                //    }
+                //}
+
+                if (!listeDoublon.Any(id => id.Equals(transaction.IdTransaction)))
                 {
-                    affichage = transaction.IdTransaction + " :KO";
-                    listeEtat.Add(affichage);
-                }
-                else if (transaction.Expediteur == "0" && transaction.Destinataire != "0")
-                {
-                    if (dicoComptes.ContainsKey(transaction.Destinataire)) // si le compte existe dans le dictionnaire de comptes
+                    if (transaction.Montant < 0)
                     {
-                        //double newSolde = dicoComptes[transaction.Destinataire] + transaction.Montant;
-                        //dicoComptes[transaction.Destinataire] = newSolde;
-                        dicoComptes[transaction.Destinataire] += transaction.Montant;
-                        Console.WriteLine($"Format 0-1 : Ajout sur le compte réussi");
-                        affichage = transaction.IdTransaction + " :OK";
-                        listeEtat.Add(affichage);
+                        listeEtat.Add(transaction.IdTransaction + ";KO");
                     }
-                    else
+                    else if (transaction.Expediteur == "0" && transaction.Destinataire != "0")
                     {
-                        affichage = transaction.IdTransaction + " :KO";
-                        listeEtat.Add(affichage);
-                    }
-                }
-                else if (transaction.Expediteur != "0" && transaction.Destinataire == "0")
-                {
-                    if (dicoComptes.ContainsKey(transaction.Expediteur)) 
-                    {
-                        dicoComptes[transaction.Expediteur] -= transaction.Montant;
-                        Console.WriteLine($"Format 1-0 : Retrait sur le compte réussi");
-                        affichage = transaction.IdTransaction + " :OK";
-                        listeEtat.Add(affichage);
-                    }
-                    else
-                    {
-                        affichage = transaction.IdTransaction + " :KO";
-                        listeEtat.Add(affichage);
-                    }
-                }
-                else if (transaction.Expediteur != "0" && transaction.Destinataire != "0")
-                {
-                    if (dicoComptes.ContainsKey(transaction.Destinataire) && dicoComptes.ContainsKey(transaction.Expediteur))
-                    {
-                        if(dicoComptes[transaction.Expediteur] >= transaction.Montant)
+                        if (dicoComptes.ContainsKey(transaction.Destinataire)) // si le compte existe dans le dictionnaire de comptes
                         {
+                            //double newSolde = dicoComptes[transaction.Destinataire] + transaction.Montant;
+                            //dicoComptes[transaction.Destinataire] = newSolde;
                             dicoComptes[transaction.Destinataire] += transaction.Montant;
-                            dicoComptes[transaction.Expediteur] -= transaction.Montant;
-                            Console.WriteLine($"Format 1-1 : Ajout et retrait sur les comptes réussi");
-                            affichage = transaction.IdTransaction + " :OK";
-                            listeEtat.Add(affichage);
+                            Console.WriteLine($"Format 0-1 : Ajout sur le compte réussi");
+                            listeEtat.Add(transaction.IdTransaction + ";OK");
                         }
                         else
                         {
-                            affichage = transaction.IdTransaction + " :KO";
-                            listeEtat.Add(affichage);
+                            listeEtat.Add(transaction.IdTransaction + ";KO");
                         }
                     }
-                    else
+                    else if (transaction.Expediteur != "0" && transaction.Destinataire == "0")
                     {
-                        affichage = transaction.IdTransaction + " :KO";
-                        listeEtat.Add(affichage);
+                        if (dicoComptes.ContainsKey(transaction.Expediteur))
+                        {
+                            dicoComptes[transaction.Expediteur] -= transaction.Montant;
+                            Console.WriteLine($"Format 1-0 : Retrait sur le compte réussi");
+                            listeEtat.Add(transaction.IdTransaction + ";OK");
+                        }
+                        else
+                        {
+                            listeEtat.Add(transaction.IdTransaction + ";KO");
+                        }
+                    }
+                    else if (transaction.Expediteur != "0" && transaction.Destinataire != "0")
+                    {
+                        if (dicoComptes.ContainsKey(transaction.Destinataire) && dicoComptes.ContainsKey(transaction.Expediteur))
+                        {
+                            if (dicoComptes[transaction.Expediteur] >= transaction.Montant)
+                            {
+                                dicoComptes[transaction.Destinataire] += transaction.Montant;
+                                dicoComptes[transaction.Expediteur] -= transaction.Montant;
+                                Console.WriteLine($"Format 1-1 : Ajout et retrait sur les comptes réussi");
+                                listeEtat.Add(transaction.IdTransaction + ";OK");
+                            }
+                            else
+                            {
+                                listeEtat.Add(transaction.IdTransaction + ";KO");
+                            }
+                        }
+                        else
+                        {
+                            listeEtat.Add(transaction.IdTransaction + ";KO");
+                        }
                     }
                 }
-                
+                else
+                {
+                    listeEtat.Add($"{transaction.IdTransaction};KO");
+                }
+
+                listeDoublon.Add(transaction.IdTransaction);
             }
 
 
@@ -107,7 +120,7 @@ namespace CompteBancaire.BusinessLogic
                 foreach (var etat in listeEtat)
                 {
                     ficSortie.WriteLine(etat);
-                }              
+                }
             }
 
             foreach (var item in dicoComptes)
